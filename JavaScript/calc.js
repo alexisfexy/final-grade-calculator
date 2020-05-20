14b809c47fda53ed86bf82b5bb18f748353e5bce
Alexis Fexy <alexisfexy@gmail.com>
2020-05-19 15:50:11 -1000

    using button instead of "input type submit"

--- JavaScript/calc.js
+++ JavaScript/calc.js
@@ -2,13 +2,17 @@ var current_grade =  document.getElementById('currentGrade');
 var desired_grade =  document.getElementById('desiredGrade');
 var finalWeight = document.getElementById('finalWeight')
 
-// grab the form first
-var form = document.getElementById('%Calc'); 
-
-// the form listens for 'submit' event 
-form.addEventListener('submit', function() {
+// if using input type= submit: 
+// var form = document.getElementById('%Calc'); 
+// form.addEventListener('click', function() 
+function calculateGrade() {
+    // check for blank entries 
     if (!current_grade.value || !desired_grade.value || !finalWeight.value) {
         alert('Please enter all fields.')  }
+    // check for non numeric
+    else if (isNaN(current_grade.value) || isNaN(desired_grade.value) || isNaN(finalWeight.value)) { 
+        alert('Please only enter numeric values.')  }
+    // correct input: do calculation 
     else {
     
         var C = parseFloat(current_grade.value); // C = Your current grade
@@ -20,18 +24,17 @@ form.addEventListener('submit', function() {
         var good_luck_message = function() {
             
             if (F <= 0) { return "Congratulations! Don't bother studying."; }
-            if (F > 0 && F <= 40) { return "Well this will be easy." }
+            if (F > 0 && F <= 40) { return "Well, this will be easy!" }
             if (F > 50 && F <= 70) { return "Nice job!" }
             if (F > 70 && F <= 90 ) { return "You can do it!" }
             if (F > 90 && F <= 100) { return "Don't give up. You got this!" }
-            else { return "Don't worry. Grades aren't everything.";}
+            else { return "Don't worry, grades aren't everything.";}
         }
         var result = "You need to score at least a " +  F.toFixed(2) 
         + "% to end your class with a grade of " 
         + D.toFixed(2) + "%. "
         + good_luck_message();
+        
         alert(result);
     }
-
-    
-});
+};
